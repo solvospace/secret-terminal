5f44fab9005b719d9b15638188e262e5fdd73dde
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-09-17 12:56:40 +0530

    refactor: replace profile icon with name first letter

--- apps/frontend/src/components/features/account/account-centre.tsx
+++ apps/frontend/src/components/features/account/account-centre.tsx
@@ -1,18 +1,31 @@
-import { useState, Dispatch, SetStateAction } from 'react';
-import { useUser } from '@/contexts/user.context';
-import { useLoading } from '@/contexts/loading.context';
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuGroup, DropdownMenuLabel, DropdownMenuShortcut, DropdownMenuSub, DropdownMenuSubTrigger, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuSubContent } from '@/components/ui/dropdown-menu';
-import { Button } from '@/components/ui/button';
-import { Spinner } from '@/components/ui/spinner';
-import { CircleUserRound } from 'lucide-react';
-import { signOut } from '@/services/authentication.service';
-import { iconSize } from '@/constants/app.constants';
-import ProfileDialog from './profile-dialog';
-import SignIn from '@/components/features/sign-in/sign-in';
+import { useState, Dispatch, SetStateAction } from "react";
+import { useUser } from "@/contexts/user.context";
+import { useLoading } from "@/contexts/loading.context";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuGroup,
+    DropdownMenuLabel,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubTrigger,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuSubContent,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import { CircleUserRound } from "lucide-react";
+import { signOut } from "@/services/authentication.service";
+import { iconSize } from "@/constants/app.constants";
+import ProfileDialog from "./profile-dialog";
+import SignIn from "@/components/features/sign-in/sign-in";
 
 type PdBindings = {
-    setShowSignInDialog: Dispatch<SetStateAction<boolean>>
-}
+    setShowSignInDialog: Dispatch<SetStateAction<boolean>>;
+};
 
 function AccountCentre() {
     const [showSignInDialog, setShowSignInDialog] = useState(false);
@@ -20,11 +33,11 @@ function AccountCentre() {
 
     return (
         <div className="account-container">
-            {
-                fetchingUser === true ?
-                    <Spinner className="size-5" />
-                    : <ProfileDropdown setShowSignInDialog={setShowSignInDialog} />
-            }
+            {fetchingUser === true ? (
+                <Spinner className="size-5" />
+            ) : (
+                <ProfileDropdown setShowSignInDialog={setShowSignInDialog} />
+            )}
 
             {
                 <SignIn
@@ -33,7 +46,7 @@ function AccountCentre() {
                 />
             }
         </div>
-    )
+    );
 }
 
 function ProfileDropdown(bindings: PdBindings) {
@@ -50,7 +63,6 @@ function ProfileDropdown(bindings: PdBindings) {
                 setUser(null);
             }
         } catch (error) {
-
         } finally {
             setIsLoading(false);
         }
@@ -58,45 +70,49 @@ function ProfileDropdown(bindings: PdBindings) {
 
     return (
         <>
-            {
-                (user && user.id) ?
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <button
-                                type="button"
-                                className="user-icon"
-                                aria-label="user"
-                            >
-                                <CircleUserRound
-                                    className="size-5"
-                                    size={iconSize}
-                                    aria-hidden="true"
-                                />
-                            </button>
-                        </DropdownMenuTrigger>
-
-                        <DropdownMenuContent
-                            align="center"
-                            className="m-[16px_5px_30px_10px]"
+            {user && user.id ? (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <button
+                            type="button"
+                            className="user-icon"
+                            aria-label="user"
                         >
-                            <DropdownMenuGroup>
-                                <DropdownMenuItem onClick={() => { setOpenProfileDialog(true) }}>
-                                    Profile
-                                </DropdownMenuItem>
+                            {user.name[0]}
+                        </button>
+                    </DropdownMenuTrigger>
 
-                                <DropdownMenuItem onClick={() => { logoutUser() }}>
-                                    Log out
-                                </DropdownMenuItem>
-                            </DropdownMenuGroup>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                    : <Button
-                        className="sign-in-btn"
-                        onClick={() => setShowSignInDialog(true)}
+                    <DropdownMenuContent
+                        align="center"
+                        className="m-[10px_5px_30px_10px]"
                     >
-                        Sign in
-                    </Button>
-            }
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem
+                                onClick={() => {
+                                    setOpenProfileDialog(true);
+                                }}
+                            >
+                                Profile
+                            </DropdownMenuItem>
+
+                            <DropdownMenuItem
+                                onClick={() => {
+                                    logoutUser();
+                                }}
+                            >
+                                Log out
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            ) : (
+                <Button
+                    className="sign-in-btn"
+                    onClick={() => setShowSignInDialog(true)}
+                >
+                    Sign in
+                </Button>
+            )}
 
             {
                 <ProfileDialog
@@ -105,7 +121,7 @@ function ProfileDropdown(bindings: PdBindings) {
                 />
             }
         </>
-    )
+    );
 }
 
 export default AccountCentre;
