f94703ef95c42aa35fb35b031869fe021a8883ae
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-09-23 11:30:35 +0530

    chore: rename website directory

--- apps/website/src/components/layout/footer.tsx
+++ apps/website/src/components/layout/footer.tsx
@@ -24,7 +24,7 @@ export function Footer() {
                 >
                     {ownerSettings.name}
                 </Link>
-                . All rights reserved.
+                .
             </p>
         </footer>
     );
